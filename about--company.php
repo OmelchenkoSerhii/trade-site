ad2bcda32354bc9fa9d37fb087f17d1d4f877420
KorniienkoIvan <kornienkoivan462@gmail.com>
2021-11-01 16:38:41 +0200

    s

--- about--company.php
+++ about--company.php
@@ -180,7 +180,7 @@
     <div class="row flex-row-reverse advantages__row ">
         <div class="advantages__contentWrapper col-lg-7">
             <div class="advantages__contentInner container">
-                <h3 class="mb-40"><?php printf( _e('Trading on the Forex market with us is:', 'trade'))?></h3>
+                <h3 class="mb-40"><?php printf( _e('Why CompanyName is trusted by experienced traders?', 'trade'))?></h3>
                 <div class="row advantages__itemRow">
                     <div class="col-md-4 col-sm-12  advantages__item about__advantages__item">
                         <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ADVANTAGES3/1.png"
