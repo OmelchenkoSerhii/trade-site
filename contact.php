8caf550f18cfaa3141e6ed00621cf6e846ef4ee7
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2021-11-01 18:02:01 +0200

    contact translate

--- contact.php
+++ contact.php
@@ -4,12 +4,12 @@
 <?php get_header(); ?>
 <section class=" contact">
             <div class="container">
-                <h3 class="mb-40">Start your trading journey now</h3>
+                <h3 class="mb-40"></h3><?php printf( _e('Start your trading journey now', 'trade'))?>
                 <div class="row contact__itemRow">
                     <div class="col-lg-4 col-sm-4  contact__item">
                         <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/contact/1.png" alt="">
                         <p class="contact__itemContent">
-                            Phone<br>
+                            <?php printf( _e('Phone', 'trade'))?><br>
                                 <span>+442038857271</span><br>
                                 <span>+74950327381</span><br>
                         </p>
@@ -17,14 +17,14 @@
                     <div class="col-lg-4 col-sm-4  contact__item">
                         <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/contact/2.png" alt="">
                         <p class="contact__itemContent">
-                            Questions by email<br>
+                            <?php printf( _e('Questions by email', 'trade'))?><br><br>
                         <span>clients@trsmarkets.com</span><br>
                         </p>
                     </div>
                     <div class="col-lg-4 col-sm-4  contact__item">
                         <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/contact/3.png" alt="">
                         <p class="contact__itemContent">
-                            Main office<br>
+                            <br><?php printf( _e('Main office', 'trade'))?><br>
                             <span>50 St. Mary Axe, London</span><br>
                             <span>Great Britain, United Kingdom</span><br>
                             <span> Postcode Ec3a 8fr</span><br>
