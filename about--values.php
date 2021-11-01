b04daa9403549e201c00c8f0caec5d02170a77c7
KorniienkoIvan <kornienkoivan462@gmail.com>
2021-11-01 16:46:04 +0200

    Update about--values.php

--- about--values.php
+++ about--values.php
@@ -27,7 +27,7 @@
     <div class="row advantages__row">
         <div class="advantages__contentWrapper col-lg-7">
             <div class="advantages__contentInner container">
-                <h3 class="mb-40"><?php printf( _e('Why do professional traders trust us?', 'trade'))?></h3>
+                <h3 class="mb-40"><?php printf( _e('Organization mission', 'trade'))?></h3>
                 <div class="advantages__content"><p><?php printf( _e('                    Our mission is to effectively evaluate the time of our traders in the fast growing, flexible
                     financial markets and use this time with the help of knowledge to the maximum benefit. Invest
                     wisely. Our passion is to get the most out of our investment and improve our results every time.', 'trade'))?>
