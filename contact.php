cabf16cd9d22c15473dc68b844a00b525c01d3aa
OmelchenkoSerhii <87645842+OmelchenkoSerhii@users.noreply.github.com>
2021-11-02 17:26:26 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,9 +4,9 @@
 <?php get_header(); ?>
 <section class=" contact">
             <div class="container">
-                <h3 class="mb-40"><?php printf( _e('Start your trading journey now', 'trade'))?></h3>
+                <h3 class="mb-40 fade-up appear"><?php printf( _e('Start your trading journey now', 'trade'))?></h3>
                 <div class="row contact__itemRow">
-                    <div class="col-lg-4 col-sm-4  contact__item">
+                    <div class="col-lg-4 col-sm-4  contact__item fade-up appear delay-1">
                         <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/contact/1.png" alt="">
                         <p class="contact__itemContent">
                             <?php printf( _e('Phone', 'trade'))?><br>
@@ -14,14 +14,14 @@
                                 <span>+74950327381</span><br>
                         </p>
                     </div>
-                    <div class="col-lg-4 col-sm-4  contact__item">
+                    <div class="col-lg-4 col-sm-4  contact__item fade-up appear delay-2">
                         <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/contact/2.png" alt="">
                         <p class="contact__itemContent">
                             <?php printf( _e('Questions by email', 'trade'))?><br><br>
                         <span>clients@trsmarkets.com</span><br>
                         </p>
                     </div>
-                    <div class="col-lg-4 col-sm-4  contact__item">
+                    <div class="col-lg-4 col-sm-4  contact__item fade-up appear delay-3">
                         <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/contact/3.png" alt="">
                         <p class="contact__itemContent">
                             <br><?php printf( _e('Main office', 'trade'))?><br>
